4a479821da42af6728d0e7431092ae3f87019707
Jan T. Sott <git@idleberg.com>
2024-01-30 00:26:24 +0100

    add callbacks

--- generators/lib/choices.mjs
+++ generators/lib/choices.mjs
@@ -6,6 +6,89 @@ export const binary = [false, true];
 export const elevation = ['user', 'highest', 'admin', 'none'];
 export const compression = ['zlib', 'bzip2', 'lzma'];
 
+export const callbacks = [
+	{
+		name: terminalLink('.onInit', 'https://github.com/NSIS-Dev/Documentation/blob/master/Callbacks/onInit.md', {
+			fallback() {
+				return '.onInit';
+			}
+		}),
+		value: '.onInit'
+	},
+	{
+		name: terminalLink('.onGUIInit', 'https://github.com/NSIS-Dev/Documentation/blob/master/Callbacks/onGUIInit.md', {
+			fallback() {
+				return '.onGUIInit';
+			}
+		}),
+		value: '.onGUIInit'
+	},
+	{
+		name: terminalLink('.onGUIEnd', 'https://github.com/NSIS-Dev/Documentation/blob/master/Callbacks/onGUIEnd.md', {
+			fallback() {
+				return '.onGUIEnd';
+			}
+		}),
+		value: '.onGUIEnd'
+	},
+	{
+		name: terminalLink('.onInstSuccess', 'https://github.com/NSIS-Dev/Documentation/blob/master/Callbacks/onInstSuccess.md', {
+			fallback() {
+				return '.onInstSuccess';
+			}
+		}),
+		value: '.onInstSuccess'
+	},
+	{
+		name: terminalLink('.onInstFailed', 'https://github.com/NSIS-Dev/Documentation/blob/master/Callbacks/onInstFailed.md', {
+			fallback() {
+				return '.onInstFailed';
+			}
+		}),
+		value: '.onInstFailed'
+	},
+	{
+		name: terminalLink('.onUserAbort', 'https://github.com/NSIS-Dev/Documentation/blob/master/Callbacks/onUserAbort.md', {
+			fallback() {
+				return '.onUserAbort';
+			}
+		}),
+		value: '.onUserAbort'
+	},
+	{
+		name: terminalLink('.onVerifyInstDir', 'https://github.com/NSIS-Dev/Documentation/blob/master/Callbacks/onVerifyInstDir.md', {
+			fallback() {
+				return '.onVerifyInstDir';
+			}
+		}),
+		value: '.onVerifyInstDir'
+	},
+	{
+		name: terminalLink('.onRebootFailed', 'https://github.com/NSIS-Dev/Documentation/blob/master/Callbacks/onRebootFailed.md', {
+			fallback() {
+				return '.onRebootFailed';
+			}
+		}),
+		value: '.onRebootFailed'
+	},
+	{
+		name: terminalLink('.onSelChange', 'https://github.com/NSIS-Dev/Documentation/blob/master/Callbacks/onSelChange.md', {
+			fallback() {
+				return '.onSelChange';
+			}
+		}),
+		value: '.onSelChange'
+	},
+	{
+		name: terminalLink('.onMouseOverSection', 'https://github.com/NSIS-Dev/Documentation/blob/master/Callbacks/onMouseOverSection.md', {
+			fallback() {
+				return '.onMouseOverSection';
+			}
+		}),
+		value: '.onMouseOverSection'
+	},
+];
+
 export const includes = [
 	{
 		name: 'Colors.nsh',
