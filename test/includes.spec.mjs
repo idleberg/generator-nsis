02a83c11a19f7451f2dd4ad0fb1e55675685f1de
Jan T. Sott <git@idleberg.com>
2024-01-29 23:07:29 +0100

    add test for all includes

--- test/includes.spec.mjs
+++ test/includes.spec.mjs
@@ -5,6 +5,26 @@ import assert from 'yeoman-assert';
 
 const includesNames = includes.map(({ name }) => name);
 
+/**
+ * alls built-ins
+ */
+const IncludeTest = suite(`includes all built-ins`);
+
+IncludeTest.before.each(() => helper({
+	includes: includesNames,
+}));
+
+includesNames.forEach(include => {
+	IncludeTest(`has !include set to ${include}`, () => {
+		assert.fileContent('installer.nsi', `!include "${include}"`);
+	});
+});
+
+IncludeTest.run();
+
+/**
+ * single built-ins
+ */
 includesNames.forEach(include => {
 	const IncludeTest = suite(`includes built-in ${include}`);
 
