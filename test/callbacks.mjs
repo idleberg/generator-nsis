fbb0b2e589336d1ae574b55896bf3cd4d00c5086
Jan T. Sott <git@idleberg.com>
2024-01-30 23:42:52 +0100

    update callback tests

--- test/callbacks.mjs
+++ test/callbacks.mjs
@@ -6,7 +6,7 @@ import assert from 'yeoman-assert';
 const callbackNames = callbacks.map(({ value }) => `${value}`);
 
 /**
- * alls callbacks
+ * all callbacks
  */
 const CallbackTest = suite(`with all callbacks`);
 
@@ -37,4 +37,60 @@ callbackNames.forEach(callback => {
 	});
 
 	CallbackTest.run();
+});
+
+/**
+ * all callbacks (MUI2)
+ */
+const CallbackMUI2Test = suite(`with all callbacks (MUI2)`);
+
+CallbackMUI2Test.before.each(() => helper({
+	callbacks: callbackNames,
+	includes: ['MUI2'],
+}));
+
+callbackNames.forEach(callback => {
+	if (callback === '.onGUIInit') {
+		CallbackMUI2Test(`has Function MUI.onGUIInit`, () => {
+			assert.fileContent('installer.nsi', `Function MUI.onGUIInit`);
+		});
+	} else if (callback === '.onUserAbort') {
+		CallbackMUI2Test(`has Function MUI.onUserAbort`, () => {
+			assert.fileContent('installer.nsi', `Function MUI.onUserAbort`);
+		});
+	} else {
+		CallbackMUI2Test(`has Function ${callback}`, () => {
+			assert.fileContent('installer.nsi', `Function ${callback}`);
+		});
+	}
+});
+
+CallbackMUI2Test.run();
+
+/**
+ * single callbacks (MUI2)
+ */
+callbackNames.forEach(callback => {
+	const CallbackMUI2Test = suite(`with ${callback}`);
+
+	CallbackMUI2Test.before.each(() => helper({
+		callbacks: [callback],
+		includes: ['MUI2'],
+	}));
+
+	if (callback === '.onGUIInit') {
+		CallbackMUI2Test(`has Function MUI.onGUIInit`, () => {
+			assert.fileContent('installer.nsi', `Function MUI.onGUIInit`);
+		});
+	} else if (callback === '.onUserAbort') {
+		CallbackMUI2Test(`has Function MUI.onUserAbort`, () => {
+			assert.fileContent('installer.nsi', `Function MUI.onUserAbort`);
+		});
+	} else {
+		CallbackMUI2Test(`has Function ${callback}`, () => {
+			assert.fileContent('installer.nsi', `Function ${callback}`);
+		});
+	}
+
+	CallbackMUI2Test.run();
 });
