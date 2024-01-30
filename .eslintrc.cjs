d47b28d7da258876153e012674fbceb4a5baa11a
Jan T. Sott <git@idleberg.com>
2024-01-31 00:55:39 +0100

    revert format

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,7 +4,10 @@ module.exports = {
 		es6: true,
 		node: true,
 	},
-	extends: ['eslint:recommended', 'plugin:json/recommended'],
+	extends: [
+		'eslint:recommended',
+		'plugin:json/recommended'
+	],
 	parserOptions: {
 		ecmaVersion: 2020,
 		sourceType: 'module',
